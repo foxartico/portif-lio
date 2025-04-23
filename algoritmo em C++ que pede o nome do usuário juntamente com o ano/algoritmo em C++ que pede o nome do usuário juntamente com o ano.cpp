82672cceb2b1e0508a0640bb428294684a1ba87d
foxartico <146464501+foxartico@users.noreply.github.com>
2025-04-23 19:48:56 -0300

    Add files via upload
    algoritmo em C++ que pede o nome do usuário juntamente com o ano
    que ele nasceu.

---
+++ algoritmo em C++ que pede o nome do usuário juntamente com o ano/algoritmo em C++ que pede o nome do usuário juntamente com o ano.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	string nome;
+	string nascimento;
+	
+	cout<< "seu nome: ";
+	cin>> nome;
+	
+	cout<< "sua data de nascimento: ";
+	cin>> nascimento;
+	
+	cout<< "ola " << nome; 
+	cout<< "    , sua data de nascimento e  " << nascimento << endl;
+	
+
+}
