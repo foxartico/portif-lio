7810c21580610ea04b34a336e8474b24425a56f4
foxartico <146464501+foxartico@users.noreply.github.com>
2025-04-23 16:58:19 -0300

    Add files via upload
    Esse exercício pede uma interação básica no usuário como seu nome sua idade e tec.

---
+++ Exercício 1 – Interação com o usuário/Exercício 1 – Interação com o usuário.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string nome; 
+	string idade; 
+	string cpf;
+	string telefone;
+	double altura;
+	double salario;
+	double peso;
+	string filho_1;
+	string filho_2;
+	string idade_filho_1;
+	string idade_filho_2;
+	string gato_nome;
+	string cachorro_nome;
+	string raca_gato;
+	string raca_cachorro;
+	string gato_sexo;
+	string cachorro_sexo;
+	double peso_gato;
+	double peso_cachorro;  
+	string cidade;
+	string estado;
+	string cep;
+	double enegia;
+	string cartao;
+	
+	cout<< "CONTE SOBRE VOCE ^_^    " << endl;
+	
+	cout<< "nome: "  ; 
+	cin>> nome;
+	
+	cout<< "idade: ";
+	cin>> idade;
+	
+	cout<< "cpf: ";
+	cin>> cpf;
+	
+	cout<< "telefone: ";
+	cin>> telefone;
+	
+	cout<< "altura: ";
+	cin>> altura;
+	
+	cout<< "salario: ";
+	cin>> salario;
+	
+	cout<< "peso: ";
+	cin>> peso;
+	
+	cout<< "nome do primeiro filho: ";
+	cin>> filho_1;
+	
+	cout<< "nome do segundo filho: ";
+	cin>> filho_2;
+	
+	cout<< "idade do primeiro filho: ";
+	cin>> idade_filho_1;
+	
+	cout<< "idade so segudo filho: " ;
+	cin>> idade_filho_2;
+	
+	cout<< "nome do gato: " ;
+	cin>> gato_nome;
+	
+	cout<< "nome do cachorro: ";
+	cin>> cachorro_nome;
+	
+	cout<< "raca do gato: ";
+	cin>> raca_gato;
+	
+	cout<< "raca do cachorro: ";
+	cin>> raca_cachorro;
+	
+	cout<< "sexo do cachorro: ";
+	cin>> cachorro_sexo;
+	
+	cout<< "sexo do gato:";
+	cin>> gato_sexo;
+	
+	cout<< "peso do gato: ";
+	cin>> peso_gato;
+	
+	cout<< "peso do cachorro: ";
+	cin>> peso_cachorro;
+	
+	cout<< "cidade:" ;
+	cin>> cidade;
+	
+	cout<< "estado: ";
+	cin>> estado;
+	
+	cout<< "valor da enegia: ";
+	cin>> enegia;
+	
+	cout<< "numero do seu cartao: ";
+	cin>> cartao;
+	
+	
+	
+	cout<< "--resumo dos dados-- "<< endl;
+	
+	cout<< "nome: " << nome << endl;
+	
+	cout<< "idade: " << idade << endl;
+	
+	cout<< "cpf: " << cpf<< endl;
+	
+	cout<< "telefone: " << telefone << endl;
+	
+	cout<< "altura: " << altura  << endl;
+	
+	cout<< "salario: " << salario << endl;
+	
+	cout<< "peso: " << peso << endl ;
+	
+	cout<< "nome do primeiro filho: " << filho_1 << endl;
+	
+	cout<< "idade do primeiro filho: " << idade_filho_1 << endl;
+	
+	cout<< "nome do segundo filho: " << filho_2 << endl;
+	
+	cout<< "idade do segundo filho: " << idade_filho_2 << endl;
+	
+	cout<< "nome do gato: " << gato_nome  << endl;
+	
+	cout<< "raca do gato: " <<raca_gato  << endl;
+	
+	cout<< "sexo do gato: " << gato_sexo << endl;
+	
+	cout<< "peso do gato: " << peso_gato  << endl;
+	
+	cout<< "nome do cachorro: " << cachorro_nome  << endl;
+	
+	cout<< "raca do cachorro: " << raca_cachorro << endl;
+	
+	cout<< "sexo do cachorro: "<< cachorro_sexo << endl;
+	
+	cout<< "peso do cachorro: " << peso_cachorro << endl;
+	
+	cout<< "sua cidade: " << cidade << endl;
+	
+	cout<< "seu estado: " << estado << endl ;
+	
+	cout<< "valor da sua enegia: " << enegia << endl;
+	
+	cout<< "numero do cartao; " << cartao << endl;
+	
+	
+
+}
