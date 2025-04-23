de17da120ab8b36e8d1534deb0f26435a4737c90
foxartico <146464501+foxartico@users.noreply.github.com>
2025-04-23 20:00:47 -0300

    Add files via upload
    Escreva um algoritmo em C++ que calcula a média aritmética de uma escola
    qualquer. Assuma que a escola tem apenas 4 notas (1 nota por bimestre).

---
+++ Calculadora de media/calculadora de media.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ double n1;
+ double n2;
+ double n3;
+ double n4;
+
+cout<< "coloque sua primeira nota: ";
+cin>> n1;
+
+cout<< "coloque sua segunda nota: ";
+cin>> n2;
+
+cout<< "coloque sua terceira nota: ";
+cin>> n3;
+
+cout<< "coloque sua quarta nota: ";
+cin>> n4;
+	
+	cout<< "sua media " << (n1+n2+n3+n4)/4;
+	
+	
+	
+	
+}
